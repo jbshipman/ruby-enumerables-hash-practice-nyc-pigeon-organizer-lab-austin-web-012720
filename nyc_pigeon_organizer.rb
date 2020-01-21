b2da3120ded6b4be3df2d85d5182b7959b8b4217
James Shipman <jbshipman@icloud.com>
2020-01-21 19:58:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require "pry"
+new_gigeon = Hash.new 
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.map do |top_key, v|
+    v.each do |sub_key1, v1|
+      v1.each do |v2|
+        new_pigeon["#{v2}"] = {"#{v1}" ={}}
+      end
+    end 
+  end
 end
+
+new_pigeon
