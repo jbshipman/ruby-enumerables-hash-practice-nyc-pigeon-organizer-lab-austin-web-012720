a8dff53ad81c1ef6c78c8688310f1e37df0bd5b0
James Shipman <jbshipman@icloud.com>
2020-01-21 20:06:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@
 # new_pigeon = Hash.new 
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object do |(top_key, v), new_pigeon|
+  data.each_with_object({}) do |(top_key, v), new_pigeon|
     v.each do |sub_key1, v1|
       v1.each do |v2|
         new_pigeon[v2] ||= {}
