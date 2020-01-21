393c9103b33f36f072c71b8760e004c8237038b5
James Shipman <jbshipman@icloud.com>
2020-01-21 20:01:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
       v1.each do |v2|
         new_pigeon[v2] ||= {}
         new_pigeon[v2][top_key] ||= []
-        new_pigeon[v2][top_key][]
+        new_pigeon[v2][top_key] << sub_key1.to_s
       end
     end 
   end
